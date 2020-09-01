b6b114c5f8fe0b1fa8c2c071942f7dab5fee2209
wubolive <31678260+wubolive@users.noreply.github.com>
2020-09-01 18:03:11 +0800

    更新py脚本

--- checkin.py
+++ checkin.py
@@ -1,36 +1,40 @@
-import requests,json,os
+import requests, json, os
 
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
 sever = os.environ["SERVE"]
-
 # 填写server酱sckey,不开启server酱则不用填
 sckey = os.environ["SCKEY"]
-
 # 填入glados账号对应cookie
 cookie = os.environ["COOKIE"]
 
 
-def start():    
-    url= "https://glados.rocks/api/user/checkin"
-    url2= "https://glados.rocks/api/user/status"
-    referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+def start():
+    checkin_url = 'https://glados.rocks/api/user/checkin'
+    status_url = 'https://glados.rocks/api/user/status'
+    referer_url = 'https://glados.rocks/console/checkin'
+
+    # 发起签到请求
+    checkin = requests.post(checkin_url, headers={'cookie': cookie, 'referer': referer_url})
+    # 获取剩余天数
+    state = requests.get(status_url, headers={'cookie': cookie, 'referer': referer_url})
 
     if 'message' in checkin.text:
-        mess = checkin.json()['message']
+        # 获取请求日志
+        message = checkin.json()['message']
+        # 获取剩余天数
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        print(time)
+        print("本次签到信息: %s , 剩余使用天数: %s" % (message, time))
         if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=' + message + '，you have ' + time + ' days left')
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        print("cookie已过期，请更换cookie信息")
+
 
 def main_handler(event, context):
   return start()
 
+
 if __name__ == '__main__':
     start()
-
-    
